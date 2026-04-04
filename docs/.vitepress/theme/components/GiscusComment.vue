01eab26b4ba96ff0245ce28016f8ccbfa688d534
wenqiang.xu <wenqiang.xu@okg.com>
2026-04-04 19:14:28 +0800

    dev
    Signed-off-by: wenqiang.xu <wenqiang.xu@okg.com>

---
+++ docs/.vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { onMounted, watch } from 'vue'
+import { useRoute } from 'vitepress'
+
+const route = useRoute()
+
+function loadGiscus() {
+  const container = document.getElementById('giscus-container')
+  if (!container) return
+
+  container.innerHTML = ''
+
+  const script = document.createElement('script')
+  script.src = 'https://giscus.app/client.js'
+  script.setAttribute('data-repo', 'robustmq/robustmq')
+  script.setAttribute('data-repo-id', 'R_kgDOKC3GIQ')
+  script.setAttribute('data-category', 'Announcements')
+  script.setAttribute('data-category-id', 'DIC_kwDOKC3GIc4C51sI')
+  script.setAttribute('data-mapping', 'pathname')
+  script.setAttribute('data-strict', '0')
+  script.setAttribute('data-reactions-enabled', '1')
+  script.setAttribute('data-emit-metadata', '0')
+  script.setAttribute('data-input-position', 'bottom')
+  script.setAttribute('data-theme', 'light')
+  script.setAttribute('data-lang', 'en')
+  script.setAttribute('crossorigin', 'anonymous')
+  script.async = true
+  container.appendChild(script)
+}
+
+onMounted(() => loadGiscus())
+
+watch(() => route.path, () => {
+  setTimeout(loadGiscus, 300)
+})
+</script>
+
+<template>
+  <div style="margin-top: 48px; padding-top: 24px; border-top: 1px solid var(--vp-c-divider);">
+    <div id="giscus-container" />
+  </div>
+</template>
--- docs/.vitepress/theme/components/Layout.vue
+++ docs/.vitepress/theme/components/Layout.vue
@@ -2,13 +2,18 @@
   <div v-if="frontmatter.layout === 'home'">
     <Home />
   </div>
-  <DefaultTheme.Layout v-else />
+  <DefaultTheme.Layout v-else>
+    <template #doc-after>
+      <GiscusComment />
+    </template>
+  </DefaultTheme.Layout>
 </template>
 
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 import Home from './Home.vue'
+import GiscusComment from './GiscusComment.vue'
 
 const { frontmatter } = useData()
 </script>
